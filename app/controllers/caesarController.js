715d3c89c93c2fa8729e2a0e4aaf095cf1ef218a
Simon Diemert <simon.diemert@gmail.com>
2015-02-26 09:41:28 -0800

    added solver to caesar cipher and fixed mod to fix a bug in JS % operator.

--- app/controllers/caesarController.js
+++ app/controllers/caesarController.js
@@ -1,5 +1,10 @@
 var caesarController = angular.module('caesarController', []); 
 
+Number.prototype.mod = function(n) {
+    return ((this%n)+n)%n;
+}
+
+
 caesarController.controller('caesarController', function($scope){
     $scope.alpha = "abcdefghijklmnopqrstuvwxyz"; 
     $scope.caesarOutputCipher = ""; 
@@ -7,11 +12,13 @@ caesarController.controller('caesarController', function($scope){
     $scope.encryptKey = 0; 
     $scope.decryptKey = 0; 
 
+    $scope.solverOutputPlain = ""; 
+
     $scope.encrypt = function(p){
         $scope.caesarOutputCipher = ""; 
         for(var i = 0; i < p.length; i++){
             p = p.toLowerCase(); 
-            $scope.caesarOutputCipher += $scope.alpha.charAt(($scope.alpha.indexOf(p.charAt(i))+$scope.encryptKey)%26); 
+            $scope.caesarOutputCipher += $scope.alpha.charAt(($scope.alpha.indexOf(p.charAt(i))+$scope.encryptKey).mod(26)); 
         }
     }; 
 
@@ -19,7 +26,21 @@ caesarController.controller('caesarController', function($scope){
         $scope.caesarOutputPlain = ""; 
         c = c.toLowerCase(); 
         for(var i = 0; i < c.length; i++){
-            $scope.caesarOutputPlain += $scope.alpha.charAt(($scope.alpha.indexOf(c.charAt(i))-$scope.decryptKey)%26); 
+            $scope.caesarOutputPlain += $scope.alpha.charAt(($scope.alpha.indexOf(c.charAt(i))-$scope.decryptKey).mod(26)); 
         }
     };
+
+    $scope.solve = function(c){
+        console.log("solve("+c+")"); 
+        $scope.solverOutputPlain = ""; 
+        var tempText = ""; 
+        for(var k = 0; k < 26; k++){
+            tempText = ""; 
+            for(var i = 0; i < c.length; i++){
+                tempText += $scope.alpha.charAt(($scope.alpha.indexOf(c.charAt(i))-k).mod(26)); 
+            }   
+            console.log("k: "+k+" temp:"+tempText+" c:"+c); 
+            $scope.solverOutputPlain += "key:"+k+" text: "+tempText+"\n"; 
+        }
+    }; 
 }); 
