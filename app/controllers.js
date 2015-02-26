e968b410bbf22343c4570700d045aa2fe9f949f5
Simon Diemert <simon.diemert@gmail.com>
2015-02-25 20:54:36 -0800

    Added basic framework (bootstrap, angular etc...) and a caesar cipher.
    Still need to break out details things into separate view.

---
+++ app/controllers.js
@@ -0,0 +1,25 @@
+var mainApp = angular.module('mainApp', []); 
+
+mainApp.controller('mainController', function($scope){
+    $scope.alpha = "abcdefghijklmnopqrstuvwxyz"; 
+    $scope.caesarOutputCipher = ""; 
+    $scope.caesarOutputPlain = ""; 
+    $scope.encryptKey = 0; 
+    $scope.decryptKey = 0; 
+
+    $scope.encrypt = function(p){
+        $scope.caesarOutputCipher = ""; 
+        for(var i = 0; i < p.length; i++){
+            p = p.toLowerCase(); 
+            $scope.caesarOutputCipher += $scope.alpha.charAt(($scope.alpha.indexOf(p.charAt(i))+$scope.encryptKey)%26); 
+        }
+    }; 
+
+    $scope.decrypt = function(c){
+        $scope.caesarOutputPlain = ""; 
+        c = c.toLowerCase(); 
+        for(var i = 0; i < c.length; i++){
+            $scope.caesarOutputPlain += $scope.alpha.charAt(($scope.alpha.indexOf(c.charAt(i))-$scope.decryptKey)%26); 
+        }
+    };
+}); 
